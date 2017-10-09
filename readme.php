61d22ead2461d274f2047871f357a555ca18e06a
isuneon <32491053+isuneon@users.noreply.github.com>
2017-10-09 17:55:59 -0400

    Actualizando el documento
    Modificando el documento

--- readme.php
+++ readme.php
@@ -11,13 +11,7 @@
  * 
  * Notas adicionales.
  * 1._ Generalmente la documentacion se realiza con phpdocumentor. En este caso 
- * la documentacion del codigo fue manual
- * 2._ Esta prueba fue bastante nutritiva aunque tambien pienso que pudo haber 
- * sido menos dificultosa. Para determinar si conocemos laravel pudo haber sido 
- * un Crud con ajax.
- * 3._ Cuando se realiza software de calidad se requiere tiempo para desarrollar
- * la aplicacion lo mas correcta posible, tiempo para probar cada una de las 
- * funcionalidades y mucho mas tiempo para documentar correctamente.
- * 
+ * la documentacion del codigo fue manual.
+ * 2._ Esta prueba fue bastante nutritiva.
  */
 
